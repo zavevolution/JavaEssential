14180c5fea44391aa818f5343f6f9bcff888d343
Zahrafov <evolutionzav@gmail.com>
2024-03-06 20:51:40 +0100

    first commit

---
+++ src/home_work/Rectangle.java
@@ -0,0 +1,22 @@
+package home_work;
+
+// Використовуючи IDEA, створіть проект із пакетом.
+// Потрібно: Створити клас із ім'ям Rectangle.
+// У тілі класу створити два поля, що описують довжини сторін double side1, double side2.
+// Створити два методи, що обчислюють площу прямокутника - double areaCalculator (double side1, double side2)
+// і периметр прямокутника - double perimeterCalculator (double side1, double side2).
+// Написати програму, яка приймає від користувача довжини двох сторін прямокутника
+// і виводить на екран периметр та площу.
+
+public class Rectangle {
+    double side1;
+    double side2;
+
+    double areaCalculator(double side1, double side2) {
+        return side1 * side2;
+    }
+
+    double perimeterCalculator(double side1, double side2) {
+        return side1 * 2 + side2 * 2;
+    }
+}
---
+++ src/home_work/TestRectangle.java
@@ -0,0 +1,29 @@
+package home_work;
+
+
+// Використовуючи IDEA, створіть проект із пакетом.
+// Потрібно: Створити клас із ім'ям Rectangle.
+// У тілі класу створити два поля, що описують довжини сторін double side1, double side2.
+// Створити два методи, що обчислюють площу прямокутника - double areaCalculator (double side1, double side2)
+// і периметр прямокутника - double perimeterCalculator (double side1, double side2).
+// Написати програму, яка приймає від користувача довжини двох сторін прямокутника
+// і виводить на екран периметр та площу.
+
+
+import java.util.Scanner;
+
+public class TestRectangle {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double side1, side2;
+        System.out.println("Введіть ширину прямокутника");
+        side1 = in.nextDouble();
+        System.out.println("Введіть висоту прямокутника");
+        side2 = in.nextDouble();
+
+        Rectangle rec = new Rectangle();
+        double perimeter = rec.perimeterCalculator(side1, side2);
+        double area = rec.areaCalculator(side1, side2);
+        System.out.println("Периметер прямокутника " + perimeter + "\nПлоща прямокутника " + area);
+    }
+}
